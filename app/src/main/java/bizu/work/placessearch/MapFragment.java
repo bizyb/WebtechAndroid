9f1c1b13a4b494714515f7940fa4daed98463121
Bizuwork Melesse <bmelesse@elifsis.com>
2018-04-25 08:32:20 -0700

    Draw map with marker on initial load

--- app/src/main/java/bizu/work/placessearch/MapFragment.java
+++ app/src/main/java/bizu/work/placessearch/MapFragment.java
@@ -1,6 +1,8 @@
 package bizu.work.placessearch;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,7 +11,15 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.constant.TransportMode;
+import com.akexorcist.googledirection.model.Direction;
+import com.akexorcist.googledirection.model.Leg;
+import com.akexorcist.googledirection.model.Route;
+import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,17 +27,23 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 import db.Database;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback{
+public class MapFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener, DirectionCallback{
 
 
     private String response;
     MapView mapView;
-    GoogleMap map;
+    GoogleMap googleMap;
     private String mapFrom;
     private String mapTo;
     private String travelMode;
@@ -35,6 +51,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     private String placeID;
     private double destLat;
     private double destLon;
+    private final String MAP_API_KEY = "AIzaSyA_NhluopOgKm1DhlpxCZebkdwgPqOfItQ";
+    private LatLng origin = new LatLng(37.7849569, -122.4068855);
+    private LatLng destination = new LatLng(37.7814432, -122.4460177);
+    private TextView btnRequestDirection;
 
 
     public MapFragment() {
@@ -68,8 +88,28 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         populateDropdown(v);
         setSpinnerListeners(v);
 
+        btnRequestDirection = v.findViewById(R.id.map_from_label);
+        btnRequestDirection.setOnClickListener(this);
+//        btnRequestDirection.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                requestDirection();
+//            }
+//        });
+
+//        requestDirection();
+//        getDirections(map);
+
 
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.map_from_label) {
+            requestDirection();
+        }
     }
 
     private void setLatLng() {
@@ -81,22 +121,105 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         destLon = Double.parseDouble(lng);
     }
 
-
     @Override
-    public void onMapReady(GoogleMap map) {
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
 
-        Log.i("onMapReady", "----placeName-----placeName-------    " + placeName);
         MapsInitializer.initialize(getActivity());
         LatLng place = new LatLng(destLat, destLon);
 
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12));
+        googleMap.setMyLocationEnabled(true);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12));
 
-        Marker marker = map.addMarker(new MarkerOptions()
+        Marker marker = googleMap.addMarker(new MarkerOptions()
                 .title(placeName)
                 .position(place));
         marker.showInfoWindow();
+
     }
+
+    @Override
+    public void onDirectionSuccess(Direction direction, String rawBody) {
+        Snackbar.make(btnRequestDirection, "Success with status : " + direction.getStatus(), Snackbar.LENGTH_SHORT).show();
+        if (direction.isOK()) {
+            Route route = direction.getRouteList().get(0);
+            googleMap.addMarker(new MarkerOptions().position(origin));
+            googleMap.addMarker(new MarkerOptions().position(destination));
+
+            ArrayList<LatLng> directionPositionList = route.getLegList().get(0).getDirectionPoint();
+            googleMap.addPolyline(DirectionConverter.createPolyline(getActivity(), directionPositionList, 5, Color.RED));
+            setCameraWithCoordinationBounds(route);
+
+            btnRequestDirection.setVisibility(View.GONE);
+        } else {
+            Snackbar.make(btnRequestDirection, direction.getStatus(), Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onDirectionFailure(Throwable t) {
+        Snackbar.make(btnRequestDirection, t.getMessage(), Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void setCameraWithCoordinationBounds(Route route) {
+        LatLng southwest = route.getBound().getSouthwestCoordination().getCoordination();
+        LatLng northeast = route.getBound().getNortheastCoordination().getCoordination();
+        LatLngBounds bounds = new LatLngBounds(southwest, northeast);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+    }
+
+    public void requestDirection() {
+
+        Snackbar.make(btnRequestDirection, "Direction Requesting...", Snackbar.LENGTH_SHORT).show();
+        GoogleDirection.withServerKey(MAP_API_KEY)
+                .from(origin)
+                .to(destination)
+                .transportMode(TransportMode.DRIVING)
+                .execute(this);
+    }
+
+//    @Override
+//    public void onMapReady(GoogleMap map) {
+//
+//        Log.i("onMapReady", "----placeName-----placeName-------    " + placeName);
+//        MapsInitializer.initialize(getActivity());
+//        LatLng place = new LatLng(destLat, destLon);
+//
+//        map.setMyLocationEnabled(true);
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12));
+//
+//        Marker marker = map.addMarker(new MarkerOptions()
+//                .title(placeName)
+//                .position(place));
+//        marker.showInfoWindow();
+//
+//    }
+
+//    public void getDirections(final GoogleMap map) {
+//
+//        LatLng origin = new LatLng(37.7849569, -122.4068855);
+//        LatLng destination = new LatLng(37.7814432, -122.4460177);
+//        GoogleDirection.withServerKey(MAP_API_KEY)
+//                .from(origin)
+//                .to(destination)
+//                .execute(new DirectionCallback() {
+//                    @Override
+//                    public void onDirectionSuccess(Direction direction, String rawBody) {
+//                        Route route = direction.getRouteList().get(0);
+//                        Leg leg = route.getLegList().get(0);
+//                        ArrayList<LatLng> directionPositionList = leg.getDirectionPoint();
+//                        PolylineOptions polylineOptions = DirectionConverter.createPolyline(getActivity(), directionPositionList, 5, Color.RED);
+//                        map.addPolyline(polylineOptions);
+//                    }
+//
+//                    @Override
+//                    public void onDirectionFailure(Throwable t) {
+//                        // Do something here
+//                    }
+//                });
+//
+//
+//    }
 
     @Override
     public void onResume() {
